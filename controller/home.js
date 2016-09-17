0cd3dbe97694dee31d1f334a8d55c56e545dde97
anyang.chen <anyang.chen@ushopal.com>
2016-09-17 23:04:57 +0800

    add mangodb

--- controller/home.js
+++ controller/home.js
@@ -4,5 +4,5 @@ mongoose.connect('mongodb://localhost/test');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
-  // we're connected!
+  console.log('ok!!!!!!!');
 });
