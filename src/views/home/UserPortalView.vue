1787e4f8a8b34ad47b60f4ad5ed10320f585c5e5
anyang.chen <anyang.chen@ushopal.com>
2016-09-28 13:01:30 +0800

    finish front end form valid

--- src/views/home/UserPortalView.vue
+++ src/views/home/UserPortalView.vue
@@ -3,27 +3,90 @@
   <h1>ChatGroup</h1>
 
     <div class="form-box form-group" v-if="hasAccount" transition="slideright">
-      <validator name="loginForm">
-      
-      <input class="form-control" type="text" name="" placeholder="手机号" v-model="loginForm.mobile" v-validate:loginForm.mobile="['required']">
+      <validator name="loginForm" @touched="hasLoginTouched = true">
+      <div class="input-box">
+        <input class="form-control" type="text" name="" placeholder="手机号" v-model="loginForm.mobile" v-validate:mobile="{ minlength: 11, maxlength: 11 }">
+        <span v-show="$loginForm.mobile.minlength && hasLoginTouched" class="glyphicon glyphicon-remove"></span>
+      </div>
 
-      <input class="form-control" type="password" name="" placeholder="密码" v-model="loginForm.password" v-validate:loginForm.password="['required']">
-      <button class="btn btn-primary">登录</button>
+      <div class="input-box">
+        <input class="form-control" type="password" name="" placeholder="密码" v-model="loginForm.password" v-validate:password="{ minlength: 6 }">
+
+        <span v-show="$loginForm.password.minlength && hasLoginTouched" class="glyphicon glyphicon-remove"></span>
+      </div>
+
+      <button class="btn btn-primary"  @click="hasLoginTouched = true">登录</button>
+
       <a class="signup" href="javascript:void(0);" @click="changeAction">没有账号？注册一个</a>
-      {{$loginForm.valid}}
+
       </validator>
     </div>
 
   <div class="form-box form-group" v-else transition="slideleft">
-    <input class="form-control" type="text" name="" placeholder="手机号">
-    <input class="form-control" type="text" name="" placeholder="昵称">
-    <input class="form-control" type="password" name="" placeholder="密码">
-    <input class="form-control" type="password" name="" placeholder="确认密码">
-    <button class="btn btn-primary">注册</button>
-    <a class="signup" href="javascript:void(0);" @click="changeAction">已有账号，去登陆</a>
+    <validator name="signForm" >
+    
+    <div class="input-box">
+      <input class="form-control" type="text" name="" placeholder="手机号" v-model="signForm.mobile" v-validate:mobile="{ minlength: 11, maxlength: 11 }">
+      <span v-show="$signForm.mobile.minlength && hasSignTouched" class="glyphicon glyphicon-remove"></span>
+      <span v-show="$signForm.mobile.maxlength && hasSignTouched" class="glyphicon glyphicon-remove"></span>
+    </div>
+    <div class="input-box">
+      <input class="form-control" type="text" name="" placeholder="昵称" v-model="signForm.nickname" v-validate:nickname="['required']">
+      <span v-show="$signForm.nickname.required && hasSignTouched" class="glyphicon glyphicon-remove"></span>
+    </div>
+    <div class="input-box">
+      <input class="form-control" type="password" name="" placeholder="密码" v-model="signForm.passwordA" v-validate:passworda="{ minlength: 6 }">
+      <span v-show="$signForm.passworda.minlength && hasSignTouched" class="glyphicon glyphicon-remove"></span>
+    </div>
+    <div class="input-box">
+      <input class="form-control" type="password" name="" placeholder="确认密码" v-model="signForm.passwordB">
+      <span v-show="signForm.passwordB !== signForm.passwordA  && hasSignTouched" class="glyphicon glyphicon-remove"></span>
+    </div>  
+      <button class="btn btn-primary" @click="hasSignTouched = true">注册</button>
+      <a class="signup" href="javascript:void(0);" @click="changeAction">已有账号，去登陆</a>
+    </validator>
   </div>
 </div>
 </template>
+
+<script>
+import Vue from 'vue';
+import VueStrap from 'vue-strap';
+
+export default {
+  data () {
+    return {
+      loginForm:{
+        mobile: '',
+        password: '',
+      },
+      signForm: {
+        mobile: '',
+        nickname: '',
+        passwordA: '',
+        passwordB: '',
+      },
+      hasLoginTouched: false,
+      hasSignTouched: false,
+      // type 1：登录 2：注册
+      hasAccount: true,
+    }
+  },
+  methods: {
+    changeAction() {
+      this.hasAccount = this.hasAccount ? false : true;
+    },
+    signUpSubmit() {
+      //
+    },
+    loginAction() {
+      //
+    },
+  },
+  component: {},
+}
+</script>
+
 <style lang="sass">
 
   .portal-container {
@@ -87,39 +150,22 @@
     opacity: 0;
   }
 
-</style>
-<script>
-import Vue from 'vue';
-import VueStrap from 'vue-strap';
+  .glyphicon {
+    float: right;
+    position: absolute;
+    top: 3px;
+    right: 2px;
+    display: inline-block;
+    color: #ea3131;
+    /* border: 1px solid #000; */
+    height: 35px;
+    width: 25px;
+    line-height: 32px;
+    background: #fff;
+  }
 
-export default {
-  data () {
-    return {
-      loginForm:{
-        mobile: '',
-        password: '',
-      },
-      signForm: {
-        mobile: '',
-        nickname: '',
-        passwordA: '',
-        passwordB: '',
-      },
-      // type 1：登录 2：注册
-      hasAccount: true,
-    }
-  },
-  methods: {
-    changeAction() {
-      this.hasAccount = this.hasAccount ? false : true;
-    },
-    signUpSubmit() {
-      //
-    },
-    loginAction() {
-      //
-    },
-  },
-  component: {},
-}
-</script>
+  .input-box {
+    position: relative;
+  }
+
+</style>
