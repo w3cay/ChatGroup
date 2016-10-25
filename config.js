e4c6bbcf34136b6876b3ae13646112854081fb74
anyang.chen <anyang.chen@ushopal.com>
2016-10-25 10:58:25 +0800

    add .

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = { 
- db: 'mongodb://10.10.72.139:27017',
+ db: 'mongodb://uYHoG8JTQx9fg4Mr:pnRe5ZTufHjCqhw3b@10.10.72.139:27017/qptoc2Fvn4QuLlU1',
  socketIp: 'http://18317893042-chatgroup.daoapp.io',
 }
